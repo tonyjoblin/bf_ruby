a7728d56b60b41c31b4fc6b014c7fd8da5f6f36f
Tony Joblin <tony.joblin@gmail.com>
2021-03-24 15:33:59 +1000

    Refactor code out of interpreter step function

--- lib/brain_fuck/interpreter.rb
+++ lib/brain_fuck/interpreter.rb
@@ -19,44 +19,25 @@ module BrainFuck
       instruction = @processor.cmd
       case instruction
       when '+'
-        @processor.inc
+        increment_data
       when '-'
-        @processor.dec
+        decrement_data
       when '>'
-        @processor.next
+        advance_data_ptr
       when '<'
-        @processor.prev
+        step_back_data_ptr
       when '.'
-        @output.putc @processor.get.chr
+        output_data_as_char
       when ','
-        @processor.set @input.getc.ord
+        input_data_from_char
       when ':'
-        @output.write "#{@processor.get} "
+        output_data_as_integer
       when ';'
         raise Brainfuck::Error.new('; not implemented')
       when '['
-        if @processor.get == 0
-          nested_block_count = 0
-          loop do
-            cmd = @processor.cmd
-            nested_block_count += 1 if cmd == '['
-            break if cmd == ']' && nested_block_count.zero?
-            nested_block_count -= 1 if cmd == ']'
-            raise Brainfuck::Error.new("move to end of loop failed, nested_block_count: #{nested_block_count}") if nested_block_count.negative?
-            raise Brainfuck::Error.new("move to end of loop failed, nested_block_count: #{nested_block_count}") if 10 < nested_block_count
-          end
-        end
+        begin_loop
       when ']'
-        nested_block_count = 0
-        @processor.rewind
-        loop do
-          cmd = @processor.rewind
-          nested_block_count += 1 if cmd == ']'
-          break if cmd == '[' && nested_block_count.zero?
-          nested_block_count -= 1 if cmd == '['
-          raise Brainfuck::Error.new("rewind to start of loop failed, nested_block_count: #{nested_block_count}") if nested_block_count.negative?
-          raise Brainfuck::Error.new("rewind to start of loop failed, nested_block_count: #{nested_block_count}") if 10 < nested_block_count
-        end
+        end_loop
       end
     end
 
@@ -64,6 +45,63 @@ module BrainFuck
       (1..MAX_STEPS).each do
         step
         break if @processor.finished?
+      end
+    end
+
+    private
+
+    def increment_data
+      @processor.inc
+    end
+
+    def decrement_data
+      @processor.dec
+    end
+
+    def step_back_data_ptr
+      @processor.prev
+    end
+
+    def advance_data_ptr
+      @processor.next
+    end
+
+    def output_data_as_char
+      @output.putc @processor.get.chr
+    end
+
+    def input_data_from_char
+      @processor.set @input.getc.ord
+    end
+
+    def output_data_as_integer
+      @output.write "#{@processor.get} "
+    end
+
+    def begin_loop
+      if @processor.get == 0
+        nested_block_count = 0
+        loop do
+          cmd = @processor.cmd
+          nested_block_count += 1 if cmd == '['
+          break if cmd == ']' && nested_block_count.zero?
+          nested_block_count -= 1 if cmd == ']'
+          raise Brainfuck::Error.new("move to end of loop failed, nested_block_count: #{nested_block_count}") if nested_block_count.negative?
+          raise Brainfuck::Error.new("move to end of loop failed, nested_block_count: #{nested_block_count}") if 10 < nested_block_count
+        end
+      end
+    end
+
+    def end_loop
+      nested_block_count = 0
+      @processor.rewind
+      loop do
+        cmd = @processor.rewind
+        nested_block_count += 1 if cmd == ']'
+        break if cmd == '[' && nested_block_count.zero?
+        nested_block_count -= 1 if cmd == '['
+        raise Brainfuck::Error.new("rewind to start of loop failed, nested_block_count: #{nested_block_count}") if nested_block_count.negative?
+        raise Brainfuck::Error.new("rewind to start of loop failed, nested_block_count: #{nested_block_count}") if 10 < nested_block_count
       end
     end
   end
